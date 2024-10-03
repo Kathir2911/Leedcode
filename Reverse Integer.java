0d979f32fc5bb33ae7c363a5c1a3685d66c6255a
Kathir2911 <146568182+Kathir2911@users.noreply.github.com>
2024-10-03 13:00:01 +0530

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -1,13 +1,15 @@
 class Solution {
     public int reverse(int x) {
-    int sum=0,rem,num=x;
+    long rev=0;
+    int num=x;
     x=Math.abs(x);
     while(x>0)  
     {
-        rem=x%10;
-        sum=sum*10+rem;
+        rev=(rev*10)+(x%10);
         x=x/10;
+        if(rev>Integer.MAX_VALUE || rev< Integer.MIN_VALUE)
+            return 0;
     }   
-    return num>0?sum:sum*-1; 
+    return num>0?(int)rev:(int)rev*-1; 
     }
 }
